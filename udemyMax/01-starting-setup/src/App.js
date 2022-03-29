5d39eaa922b61d9b152815e4e5edda81245b4c88
Bartlomiej Wilczynski <72442528+BWilczynski98@users.noreply.github.com>
2022-03-29 10:22:29 +0200

    New thing "useState", and created component where the  user can add expenses

--- udemyMax/01-starting-setup/src/App.js
+++ udemyMax/01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense"
 
 const expenses = [
   {
@@ -26,6 +27,7 @@ const expenses = [
 function App() {
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div> 
   );
