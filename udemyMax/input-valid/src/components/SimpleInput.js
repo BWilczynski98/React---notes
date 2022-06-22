f1c310e45bd89c8d6720de7e1c561ca575400b0d
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2022-06-22 15:58:45 +0200

    finished code

--- udemyMax/input-valid/src/components/SimpleInput.js
+++ udemyMax/input-valid/src/components/SimpleInput.js
@@ -1,12 +1,44 @@
+import React, { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [userTouched, setUserTouched] = useState(false);
+  const [userFocus, setUserFocus] = useState(false)
+
+  const nameIsInvalid = !enteredNameIsValid && userTouched && !userFocus;
+  const inputClass = nameIsInvalid ? 'form-control invalid' : 'form-control';
+
+  const submitFunction = (e) => {
+    e.preventDefault();
+    setUserTouched(true)
+    setUserFocus(false)
+    if (name.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    } else {
+      setEnteredNameIsValid(true);
+    };
+    setName('')
+  };
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitFunction}>
+      <div className={inputClass}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={name}
+          onChange={input => {
+            setName(input.target.value);
+            setUserFocus(true)
+          }}
+        />
+        {nameIsInvalid && <p className='error-text'>The name cannot be empty!</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type='submit'>Submit</button>
       </div>
     </form>
   );
